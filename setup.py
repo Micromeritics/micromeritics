5b271d6a194578f0a4345f530a4225c3acae2c93
Low Kian Seong <kianseong@gmail.com>
2015-08-28 22:20:52 +0800

    Adding `description` to `setup.py`
    Adding `description` to `setup.py`. This will be picked up automatically when uploading to `pypi` so that project will not be listed as `UNKNOWN`

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.1.1dev',
     packages=['micromeritics',],
     maintainer='Stefan Koch',
+    description='Python module to do reporting calculations',
     maintainer_email='stefan.koch.micro@gmail.com', 
     url='https://github.com/Micromeritics/micromeritics',
     license='GNU GENERAL PUBLIC LICENSE Version 3',
