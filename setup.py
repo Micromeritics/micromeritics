387147911d04d9332d2b2f7c2870af0afe2c300f
crmurdock6 <christopher@numat-tech.com>
2017-07-27 14:59:47 -0500

    Added function to read excel reports

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='micromeritics',
-    version='0.2.0',
-    packages=['micromeritics',],
+    version='0.2.1',
+    packages=['micromeritics'],
     maintainer='Stefan Koch',
     description='Python package for gas adsorption calculations and support tools.',
-    maintainer_email='stefan.koch.micro@gmail.com', 
+    maintainer_email='stefan.koch.micro@gmail.com',
     url='https://github.com/Micromeritics/micromeritics',
     license='GNU GENERAL PUBLIC LICENSE Version 3',
     long_description=open('README.md').read(),
